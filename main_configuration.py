b406ec6055818905a15f04d37d5154468f01d119
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-07-24 18:58:12 -0300

    add

--- main_configuration.py
+++ main_configuration.py
@@ -2,7 +2,7 @@ import tkinter
 
 window = tkinter.Tk()
 window.geometry("800x600")
-etiquet = tkinter.Label(window, text="aplication", bg="black")
-etiquet.pack(fill=tkinter.X)
+etiquet = tkinter.Label(window, text="aplication", bg="red")
+etiquet.pack(fill=tkinter.BOTH, expand=True)
 
 window.mainloop()

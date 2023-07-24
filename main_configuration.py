07a818b82ddcd1a74809f7bb6e282276c8ce2c84
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-07-24 19:01:06 -0300

    adding

--- main_configuration.py
+++ main_configuration.py
@@ -2,7 +2,10 @@ import tkinter
 
 window = tkinter.Tk()
 window.geometry("800x600")
-etiquet = tkinter.Label(window, text="aplication", bg="red")
-etiquet.pack(fill=tkinter.BOTH, expand=True)
+# etiquet = tkinter.Label(window, text="aplication", bg="red")
+# etiquet.pack(fill=tkinter.BOTH, expand=True)
+
+button1 = tkinter.Button(window, text="aceptar", padx=30, pady=30)
+button1.pack()
 
 window.mainloop()

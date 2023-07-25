8f36c792f7becd4a66cecb6407c9ef82f49f16b2
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-07-25 17:58:51 -0300

    adding

--- main_configuration.py
+++ main_configuration.py
@@ -5,7 +5,14 @@ window = Tk()
 window.geometry("800x600")
 window.title("Weather application")
 window.mainloop()
-
-text = Entry(window, font=("Arial", 30, "normal"), justify="center")
-text.pack(padx=40, pady=40)
 # base configuration
+
+
+show_text = Entry(window, font=("Courier", 30, "normal"), justify="center")
+show_text.pack(padx=40, pady=40)
+
+obtain_information = Button(window, text="Obtener el clima")
+obtain_information.pack()
+
+show_weather = Label(text="weather", font=("Courier", 40, "normal"))
+show_weather.pack()
